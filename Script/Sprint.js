927dfe10ef71712f1f9a3eed2d3060165f6b3024
[Max] <Zholdashev.Maks@yandex.ru>
2024-11-17 12:48:51 +0300

    HotFix Sprint

--- Script/Sprint.js
+++ Script/Sprint.js
@@ -4,6 +4,7 @@ let totalTime;
 
 const startButton = document.getElementById('startButton');
 const stopButton = document.getElementById('stopButton');
+const resetButton = document.getElementById('resetButton');
 const timeDisplay = document.getElementById('timeDisplay');
 const minutesInput = document.getElementById('minutes');
 const secondsInput = document.getElementById('seconds');
@@ -11,10 +12,17 @@ const progressBar = document.querySelector('.progress');
 
 startButton.addEventListener('click', startTimer);
 stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
 
 function startTimer() {
     const minutes = parseInt(minutesInput.value) || 0;
     const seconds = parseInt(secondsInput.value) || 0;
+
+    if (isNaN(minutes) || isNaN(seconds) || minutes < 0 || seconds < 0 || seconds >= 60) {
+        alert('Введите корректные значения для минут и секунд.');
+        return;
+    }
+    
     remainingTime = totalTime = minutes * 60 + seconds;
 
     if (remainingTime > 0) {
@@ -39,12 +47,24 @@ function stopTimer() {
     clearInterval(timerInterval);
 }
 
+function resetTimer() {
+    clearInterval(timerInterval);
+    minutesInput.value = '';
+    secondsInput.value = '';
+    timeDisplay.textContent = '00:00';
+    progressBar.style.width = '0%';
+
+    // Сбрасываем оставшееся время и вызываем обновление шкалы прогресса
+    remainingTime = 0;
+    updateProgressBar();
+}
+
 function updateProgressBar() {
     if (remainingTime > 0) {
         const progress = ((totalTime - remainingTime) / totalTime) * 100;
         progressBar.style.width = `${progress}%`;
         requestAnimationFrame(updateProgressBar);
     } else {
-        progressBar.style.width = `100%`;
+        progressBar.style.width = '0%'; // Убедимся, что шкала прогресса обнуляется при завершении
     }
 }
